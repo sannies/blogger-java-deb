4402fe6a2519dcd39e190355440b91f9c1ed34ba
Sebastian Annies <sebastian.annies@googlemail.com>
2012-07-01 23:31:26 +0200

    this is a fully working java debian package

---
+++ src/main/java/com/github/sannies/bloggerjavadeb/TellMeSomethingNice.java
@@ -0,0 +1,7 @@
+package com.github.sannies.bloggerjavadeb;
+
+public class TellMeSomethingNice {
+    public static void main(String[] args) {
+        System.out.println("You are so beautiful.");
+    }
+}
